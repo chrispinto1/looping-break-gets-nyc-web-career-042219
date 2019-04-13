5c101b609e6e37f5649b1e59aa7bfa1867122d8e
Christian Pinto <pintochristian2@gmail.com>
2019-04-13 21:31:32 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,9 +2,9 @@
 def levitation_quiz
 	def annoying
     loop do 
-        puts ""
+        puts "What is the spell that enacts levitation?"
         answer = gets.chomp
-        break if answer == "STOP"
+        break if answer == ""
     end
     puts "Okay, okay, jeez. I'll stop. Sorry."
 end
