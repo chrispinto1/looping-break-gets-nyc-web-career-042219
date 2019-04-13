b9f90a5eca27549bffdf8f6e7ad60e238e80cd89
Christian Pinto <pintochristian2@gmail.com>
2019-04-13 21:31:18 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,14 @@
 
 def levitation_quiz
-	#your code here
+	def annoying
+    loop do 
+        puts ""
+        answer = gets.chomp
+        break if answer == "STOP"
+    end
+    puts "Okay, okay, jeez. I'll stop. Sorry."
+end
+ 
 end
 
 
